b25523d2e0ad99fb1df8fe1ddebc86de816328e0
opoczan <jakubopoka@MacBook-Pro-opoczan.local>
2025-03-19 16:24:16 +0100

    nagrywanie kanalow

--- wsei_js3/main.js
+++ wsei_js3/main.js
@@ -8,16 +8,95 @@ const sounds = {
 	d: kickSound,
 };
 
-const channel1 = [];
+const channels = [[], [], [], []];
+let isRecording = [false, false, false, false];
+let startTime = 0;
+
 document.addEventListener("keypress", (event) => {
-	channel1.push({
-		key: event.key,
-		time: Date.now(),
-	});
-	playSound(sounds[event.key]);
+	if (sounds[event.key]) {
+		playSound(sounds[event.key]);
+		recordSound(event.key);
+	}
 });
 
 function playSound(sound) {
 	sound.currentTime = 0;
 	sound.play();
 }
+
+function recordSound(key) {
+	const time = Date.now() - startTime;
+	isRecording.forEach((recording, index) => {
+		if (recording) {
+			channels[index].push({ key, time });
+		}
+	});
+}
+
+function startRecording(channel) {
+	channels[channel] = [];
+	isRecording[channel] = true;
+	startTime = Date.now();
+}
+
+function stopRecording(channel) {
+	isRecording[channel] = false;
+}
+
+function playChannel(channel) {
+	channels[channel].forEach((note) => {
+		setTimeout(() => {
+			playSound(sounds[note.key]);
+		}, note.time);
+	});
+}
+
+function playAllChannels() {
+	channels.forEach((channel) => {
+		channel.forEach((note) => {
+			setTimeout(() => playSound(sounds[note.key]), note.time);
+		});
+	});
+}
+
+document
+	.querySelector("#record1")
+	.addEventListener("click", () => startRecording(0));
+document
+	.querySelector("#stop1")
+	.addEventListener("click", () => stopRecording(0));
+document
+	.querySelector("#play1")
+	.addEventListener("click", () => playChannel(0));
+
+document
+	.querySelector("#record2")
+	.addEventListener("click", () => startRecording(1));
+document
+	.querySelector("#stop2")
+	.addEventListener("click", () => stopRecording(1));
+document
+	.querySelector("#play2")
+	.addEventListener("click", () => playChannel(1));
+
+document
+	.querySelector("#record3")
+	.addEventListener("click", () => startRecording(2));
+document
+	.querySelector("#stop3")
+	.addEventListener("click", () => stopRecording(2));
+document
+	.querySelector("#play3")
+	.addEventListener("click", () => playChannel(2));
+
+document
+	.querySelector("#record4")
+	.addEventListener("click", () => startRecording(3));
+document
+	.querySelector("#stop4")
+	.addEventListener("click", () => stopRecording(3));
+document
+	.querySelector("#play4")
+	.addEventListener("click", () => playChannel(3));
+
+document.querySelector("#playAll").addEventListener("click", playAllChannels);
